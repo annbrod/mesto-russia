90f9b3744cdc6dbf87600efb399778f41ef3d3a9
Anastasia Tulasova <annbrod73@gmail.com>
2020-02-02 17:28:11 +0400

    add babel config

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,17 @@
-
 const path = require('path');
-// подключаем path к конфигу вебпак
 
 module.exports = {
     entry: { main: './src/index.js' },
     output: {
         path: path.resolve(__dirname, 'dist'),
         filename: 'main.js'
+    },
+    module: {
+        rules: [{
+            test: /\.js$/,
+            use: { loader: "babel-loader" },
+            exclude: /node_modules/
+        }
+        ]
     }
-}
+};
